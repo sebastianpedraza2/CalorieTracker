29ceedac5ab719811e3d803a659e2f4f50526a6a
Sebastian Pedraza <sebastian29pm@gmail.com>
2022-11-10 16:30:20 -0500

    Add Navigation compose logic

--- app/src/main/java/com/pedraza/sebastian/calorietracker/MainActivity.kt
+++ app/src/main/java/com/pedraza/sebastian/calorietracker/MainActivity.kt
@@ -3,14 +3,14 @@ package com.pedraza.sebastian.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.pedraza.sebastian.calorietracker.navigation.navigate
 import com.pedraza.sebastian.calorietracker.ui.theme.CalorieTrackerTheme
+import com.pedraza.sebastian.core.navigation.Route
 import com.pedraza.sebastian.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +18,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    WelcomeScreen()
+
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Route.WELCOME) {
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE) {
+
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
                 }
             }
         }
@@ -35,12 +62,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     CalorieTrackerTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            WelcomeScreen()
-        }
+
     }
 }
---
+++ core/src/main/java/com/pedraza/sebastian/core/navigation/Route.kt
@@ -0,0 +1,14 @@
+package com.pedraza.sebastian.core.navigation
+
+object Route {
+    const val WELCOME = "welcome"
+    const val NUTRIENT_GOAL = "nutrient_goal"
+    const val TRACKER_OVERVIEW = "tracker_overview"
+    const val AGE = "age"
+    const val GENDER = "gender"
+    const val HEIGHT = "height"
+    const val WEIGHT = "weight"
+    const val ACTIVITY = "activity"
+    const val GOAL = "goal"
+    const val SEARCH = "search"
+}
