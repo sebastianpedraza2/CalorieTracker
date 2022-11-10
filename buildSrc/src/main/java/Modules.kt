8e8cae95c0ca63f056071fe809a1f1a2020e2dbd
Sebastian Pedraza <sebastian29pm@gmail.com>
2022-11-09 19:31:10 -0500

    Add hybrid modularization and migration to kotlin DSL

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":feature-onboarding:onboarding-domain"
+    const val onboardingPresentation = ":feature-onboarding:onboarding-presentation"
+
+    const val trackerData = ":feature-tracker:tracker-data"
+    const val trackerDomain = ":feature-tracker:tracker-domain"
+    const val trackerPresentation = ":feature-tracker:tracker-presentation"
+}
